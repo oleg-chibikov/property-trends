6b5cf66a8701e0b8b1db5c9cc8cd9daa49ed3698
Oleg Chibikov <oleg.chibikov@wisetechglobal.com>
2020-06-14 09:33:02 +1000

    Basic map processing

--- src/features/realEstateMap/RealEstateMap.tsx
+++ src/features/realEstateMap/RealEstateMap.tsx
@@ -1,26 +1,58 @@
 import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import {
   Map,
   GoogleApiWrapper,
   IProvidedProps,
   IMapProps,
 } from 'google-maps-react';
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  selectCount,
-} from './realEstateMapSlice';
-import styles from './RealEstateMap.module.css';
-import { Counter } from '../counter/Counter';
+const LoadingContainer = (props: any) => <div>Fancy loading container!</div>;
 class RealEstateMap extends React.Component<IMapProps> {
+  useTheData(doc: any) {}
+  fetchPlaces(mapProps?: IMapProps, map?: google.maps.Map, event?: any) {
+    const infoWindow = new google.maps.InfoWindow({
+      maxWidth: 500,
+    });
+
+    var parser = require('../../custom/geoxml3.js');
+    const geoXml = new parser.default({
+      map: map,
+      singleInfoWindow: true,
+      afterParse: this.useTheData,
+      // @ts-ignore
+      polygonOptions: {
+        clickable: true,
+        strokeColor: 'black',
+        strokeWeight: 1,
+        strokeOpacity: 1,
+        fillColor: 'Transparent',
+        fillOpacity: 0.7,
+      },
+      //createMarker: createMarker,
+      //pmParseFn: parsePlacemark
+      //createMarker: addMyMarker,
+      //createPolygon: addMyPolygon
+    });
+
+    //geoXml.parse('aus_suburb_kml-master/TAS/SOUTHWEST.kml');
+    //geoXml.parse('aus_suburb_kml-master/TAS/RISDON.kml');
+    //geoXml.parse('aus_suburb_kml-master/TAS/TROWUTTA.kml');
+    //geoXml.parse('aus_suburb_kml-master/TAS/HOBART.kml');
+    //geoXml.parse('aus_suburb_kml-master/TAS/North HOBART.kml');
+    //geoXml.parse('aus_suburb_kml-master/TAS/Battery point.kml');
+    //geoXml.parse('aus_suburb_kml-master/TAS/TARLETON.kml');
+    //geoXml.parse('aus_suburb_kml-master/TAS/SOUTHWEST.kml');
+    // @ts-ignore
+    geoXml.parse(
+      process.env.PUBLIC_URL + '/aus_suburb_kml-master/NSW/APPIN.kml'
+    );
+  }
+
   render() {
-    return <Counter />;
+    return <Map onReady={this.fetchPlaces} {...this.props}></Map>;
   }
 }
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyBCctYFKHEVCC5yaFkbCAjNwgzMGn923hQ',
+  LoadingContainer: LoadingContainer,
 })(RealEstateMap);
