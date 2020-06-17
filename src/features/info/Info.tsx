1b6c5cc9939ea6c5d12949895f516c62738f9776
Oleg Chibikov <oleg.chibikov@wisetechglobal.com>
2020-06-17 11:53:31 +1000

    Styling fixes

--- src/features/info/Info.tsx
+++ src/features/info/Info.tsx
@@ -6,14 +6,16 @@ interface InfoProps extends RealEstateResponse {}
 
 const Info: React.FunctionComponent<InfoProps> = (props) => (
   <div className={styles.info}>
-    <h1>{props.locality}</h1>
-    <div>Post Code: {props.postCode}</div>
-    <div>Min: ${props.minPrice}</div>
-    <div>Median: ${props.medianPrice}</div>
-    <div>Average: ${props.averagePrice}</div>
-    <div>95 percentile: ${props.percentile95Price}</div>
-    <div>Max: ${props.maxPrice}</div>
-    <div>Count: {props.count}</div>
+    <h4>{props.locality}</h4>
+    {props.postCode && <div>Post Code: {props.postCode}</div>}
+    {props.minPrice && <div>Min: ${props.minPrice}</div>}
+    {props.medianPrice && <div>Median: ${props.medianPrice}</div>}
+    {props.averagePrice && <div>Average: ${props.averagePrice}</div>}
+    {props.percentile95Price && (
+      <div>95 percentile: ${props.percentile95Price}</div>
+    )}
+    {props.maxPrice && <div>Max: ${props.maxPrice}</div>}
+    {props.count && <div>Count: {props.count}</div>}
   </div>
 );
 
